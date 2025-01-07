05303e0dcbb3956c615e5819169ef42b2b3f9fe7
Jason Madar <env3d@users.noreply.github.com>
2025-01-07 19:51:15 +0000

    submit

--- tests/test_assignment.py
+++ tests/test_assignment.py
@@ -1,40 +1,9 @@
 import os
 import pytest
 import re
-    
-def test_sql_1_sql():
-  result = os.popen(f'sqlite3 *.db < 1.sql | md5sum').read()
-  expected = os.popen(f'cat tests/1.sql.md5').read()
-  print(result, expected)
-  assert result == expected
 
-    
-def test_sql_2_sql():
-  result = os.popen(f'sqlite3 *.db < 2.sql | md5sum').read()
-  expected = os.popen(f'cat tests/2.sql.md5').read()
-  assert result == expected
-
-    
-def test_sql_3_sql():
-  result = os.popen(f'sqlite3 *.db < 3.sql | md5sum').read()
-  expected = os.popen(f'cat tests/3.sql.md5').read()
-  assert result == expected
-
-    
-def test_sql_4_sql():
-  result = os.popen(f'sqlite3 *.db < 4.sql | md5sum').read()
-  expected = os.popen(f'cat tests/4.sql.md5').read()
-  assert result == expected
-
-    
-def test_sql_5_sql():
-  result = os.popen(f'sqlite3 *.db < 5.sql | md5sum').read()
-  expected = os.popen(f'cat tests/5.sql.md5').read()
-  assert result == expected
-
-    
-def test_sql_6_sql():
-  result = os.popen(f'sqlite3 *.db < 6.sql | md5sum').read()
-  expected = os.popen(f'cat tests/6.sql.md5').read()
-  assert result == expected
-
+@pytest.mark.parametrize("sql_file", [ f for f in os.listdir(".") if re.search(r'\d{1,2}.sql', f) ])
+def test_sql(sql_file):
+    result = os.popen(f'sqlite3 textbook.db < {sql_file} | md5sum').read()
+    expected = os.popen(f'cat tests/{sql_file}.md5').read()
+    assert result == expected
