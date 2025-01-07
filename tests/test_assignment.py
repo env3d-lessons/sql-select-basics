216a903c9a4c8ccd596cc9fee623dab6d793547e
Jason Madar <env3d@users.noreply.github.com>
2025-01-07 18:38:59 +0000

    update tests

--- tests/test_assignment.py
+++ tests/test_assignment.py
@@ -2,8 +2,11 @@ import os
 import pytest
 import re
 
-@pytest.mark.parametrize("sql_file", [ f for f in os.listdir(".") if re.search(r'\d{1,2}.sql', f) ])
+# Generate the list of files once
+sql_files = [f for f in os.listdir(".") if re.search(r'\d{1,2}.sql', f)]
+
+@pytest.mark.parametrize("sql_file", sql_files)
 def test_sql(sql_file):
-    result = os.popen(f'sqlite3 *.db < {sql_file} | md5sum').read()
-    expected = os.popen(f'cat tests/{sql_file}.md5').read()
+    result = os.popen(f'sqlite3 *.db < {sql_file} | md5sum').read().strip()
+    expected = os.popen(f'cat tests/{sql_file}.md5').read().strip()
     assert result == expected
